6990ffbab3bd055c876f49870566b9a1b7848486
shreyansh1365 <shreyansh1365@gmail.com>
2026-02-20 11:00:51 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println("   PALINDROME CHECKER SYSTEM v1.0   ");
+        System.out.println("=====================================");
+        System.out.println("Welcome to the Palindrome Checker!");
+        System.out.println("Application started successfully.");
+        System.out.println();
+    }
+}
