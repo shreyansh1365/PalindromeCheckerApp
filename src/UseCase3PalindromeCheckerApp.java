60234b90355968323c5ec98b4da78753e1f6b275
shreyansh1365 <shreyansh1365@gmail.com>
2026-02-20 16:27:39 +0530

    Base application setup

---
+++ src/UseCase3PalindromeCheckerApp.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class UseCase3PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+
+        if (input.equals(reversed)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is not a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
